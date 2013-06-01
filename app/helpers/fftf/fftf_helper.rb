6017acc8bbb722a4a98a8ed164af06b7d80774f6
bpfarmer <bpfarmer@gmail.com>
2013-06-01 14:30:15 -0400

    syntax error

--- app/helpers/fftf/fftf_helper.rb
+++ app/helpers/fftf/fftf_helper.rb
@@ -5,7 +5,7 @@ module FftfHelper
   def clean_salsa_params(params)
     @movement = Movement.find_or_create_by_name(params[:user][:movement_name])
     params[:user].merge!({:movement_id => @movement.id})
-    params[:user].delete({:movement_name})
+    params[:user].delete(:movement_name)
     return params[:user]
   end
 end
