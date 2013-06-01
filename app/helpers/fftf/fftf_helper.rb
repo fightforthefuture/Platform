ecbac7c7794f101b0c9d68af64dc1ee0586695c1
bpfarmer <bpfarmer@gmail.com>
2013-06-01 14:50:21 -0400

    syntax error

--- app/helpers/fftf/fftf_helper.rb
+++ app/helpers/fftf/fftf_helper.rb
@@ -1,11 +1,13 @@
-module FftfHelper
+module Fftf
+  module Fftf::FftfHelper
   
-  # Accepts a hash of parameters and cleans
-  # them for user creation
-  def clean_salsa_params(params)
-    @movement = Movement.find_or_create_by_name(params[:user][:movement_name])
-    params[:user].merge!({:movement_id => @movement.id})
-    params[:user].delete(:movement_name)
-    return params[:user]
+    # Accepts a hash of parameters and cleans
+    # them for user creation
+    def clean_salsa_params(params)
+      @movement = Movement.find_or_create_by_name(params[:user][:movement_name])
+      params[:user].merge!({:movement_id => @movement.id})
+      params[:user].delete(:movement_name)
+      return params[:user]
+    end
   end
 end
