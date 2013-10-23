dc5f822b5d12fcd8367531ff413398a3011140cf
Noel Welsh <noel@untyped.com>
2013-10-23 14:19:35 +0100

    Correct class name of migration

--- db/migrate/20130926133717_add_batch_number_to_push_sent_emails.rb
+++ db/migrate/20130926133717_add_batch_number_to_push_sent_emails.rb
@@ -1,4 +1,4 @@
-class AddBatchNumberToPushes < ActiveRecord::Migration
+class AddBatchNumberToPushSentEmails < ActiveRecord::Migration
   def change
     add_column :push_sent_emails, :batch_number, :integer
   end
