b1c875007d63f7dc905ef137dc6888cdb0b720a7
Chris Antaki <ChrisAntaki@gmail.com>
2014-01-07 13:51:29 -0800

    Testing fix

--- app/controllers/api/members_controller.rb
+++ app/controllers/api/members_controller.rb
@@ -44,7 +44,26 @@ class Api::MembersController < Api::BaseController
 
     movement = Movement.find(1)
 
-    @page = FightForTheFuture.find_or_create_action_page_by_tag(tag)
+    unless @page = ActionPage.find_by_name(tag)
+      campaign = Campaign.find_by_name('CMS')
+      
+      @page = ActionPage.create(
+        name: tag,
+        movement_id: 1,
+        type: 'ActionPage',
+        action_sequence_id: campaign.action_sequences.first.id
+      )
+
+      petition = PetitionModule.create!(
+        :title => "Sign, please",
+        :content => 'We the undersigned...',
+        :petition_statement => "This is the petition statement",
+        :signatures_goal => 1,
+        :thermometer_threshold => 0,
+        :language => Language.find_by_iso_code(:en)
+      )
+      ContentModuleLink.create!(:page => @page, :content_module => petition, :position => 3, :layout_container => :main_content)
+    end
     
     member_params = params[:member].merge({'language' => Language.find_by_iso_code(params[:member][:language])})
     
