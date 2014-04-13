8674d728bd106bafd2375a563aeadce76d04d9c5
Scott Feinberg <feinberg.scott@gmail.com>
2014-04-13 23:34:01 +0200

    remove ENV variables from the delayed jobs config. It's just unnecessary

--- config/initializers/delayed_jobs_config.rb
+++ config/initializers/delayed_jobs_config.rb
@@ -10,7 +10,7 @@ Delayed::Worker.default_queue_name = ENV['DEFAULT_QUEUE'] || "default"
 
 module QueueConfigs
 
-  BLASTER_QUEUE = ENV['LIST_CUTTER_BLASTER_QUEUE'] || "blaster"
-  LIST_CUTTER_QUEUE = ENV['LIST_CUTTER_BLASTER_QUEUE'] || "list_cutter"
-  HIGH_QUEUE = ENV['LIST_CUTTER_BLASTER_QUEUE'] || "high"
+  BLASTER_QUEUE = "blaster"
+  LIST_CUTTER_QUEUE = "list_cutter"
+  HIGH_QUEUE = "high"
 end
