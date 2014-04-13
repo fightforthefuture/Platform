3a24744956b455aec0ed90533f51652a7815d25a
Scott Feinberg <feinberg.scott@gmail.com>
2014-04-14 01:37:41 +0200

    Add blaster to hirefire queues

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -6,4 +6,8 @@ HireFire::Resource.configure do |config|
   config.dyno(:high_priority) do
     HireFire::Macro::Delayed::Job.queue(:high, mapper: :active_record)
   end
+
+  config.dyno(:blaster) do
+    HireFire::Macro::Delayed::Job.queue(:blaster, mapper: :active_record)
+  end
 end
