91ca8dc6be877d147744671127ecc1f53d8d5ca0
Noel Welsh <noel@untyped.com>
2014-04-14 14:05:10 +0100

    Revert "remove ENV variables from the delayed jobs config. It's just unnecessary"
    This reverts commit 8674d728bd106bafd2375a563aeadce76d04d9c5.

--- config/initializers/delayed_jobs_config.rb
+++ config/initializers/delayed_jobs_config.rb
@@ -9,8 +9,7 @@ Delayed::Worker.default_queue_name = ENV['DEFAULT_QUEUE'] || "default"
 
 
 module QueueConfigs
-
-  BLASTER_QUEUE = "blaster"
-  LIST_CUTTER_QUEUE = "list_cutter"
-  HIGH_QUEUE = "high"
+  BLASTER_QUEUE = ENV['BLASTER_QUEUE'] || "blaster"
+  LIST_CUTTER_QUEUE = ENV['LIST_CUTTER_QUEUE'] || "list_cutter"
+  HIGH_QUEUE = ENV['LIST_CUTTER_BLASTER_QUEUE'] || "high"
 end
