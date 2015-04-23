136ed59c8e48e100bc2ce17684d5846962bee24d
Jeff Lyon <jeff@rubbingalcoholic.com>
2015-04-23 11:46:57 -0700

    removed tracking image

--- app/models/email_footer.rb
+++ app/models/email_footer.rb
@@ -24,7 +24,7 @@ class EmailFooter < ActiveRecord::Base
 
   def html_with_beacon
     base_url = movement.url.starts_with?("http://", "https://") ? movement.url : "http://#{movement.url}"
-    self.html + %{<img src="#{base_url}/beacon.gif?t={TRACKING_HASH|NOT_AVAILABLE}">}.html_safe
+    # self.html + %{<img src="#{base_url}/beacon.gif?t={TRACKING_HASH|NOT_AVAILABLE}">}.html_safe
   end
 
   def html
